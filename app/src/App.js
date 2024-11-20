ba0fc77e18d96bc2360ac8fc6d24a4c0988e647f
udaykiranhub <peddaudaykiran6@gmail.com>
2024-11-20 20:04:42 +0530

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import IPFSUpload from './pinata';
+
+function App() {
+  return (
+    <div className="App">
+   <h2>Decentralized Off Chain storage!</h2>
+<IPFSUpload/>
+
+    </div>
+  );
+}
+
+export default App;
